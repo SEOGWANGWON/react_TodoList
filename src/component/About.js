1ec15b31058a2389b4474f4704b6a7c4b8b3328e
SEOGWANGWON <tjrhkdrns2530@gmail.com>
2023-12-20 11:34:30 +0900

    11:34

---
+++ src/component/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import image1 from '../img/image1.jpg';
+
+function About(){
+    return(
+        <div>
+            <img src={image1}/>
+            <p>하기 싫어도 하는남자 그것이 나다.</p>
+        </div>
+    )
+}
+
+export default About;
